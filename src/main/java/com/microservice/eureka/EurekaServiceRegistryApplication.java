dbfd8c9671bb29955bd3231dccd4f159cb07400c
msarvani <sarvani135@gmail.com>
2020-04-09 21:58:49 -0500

    Initial Commit

---
+++ src/main/java/com/microservice/eureka/EurekaServiceRegistryApplication.java
@@ -0,0 +1,18 @@
+package com.microservice.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceRegistryApplication extends ServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceRegistryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/microservice/eureka/EurekaServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
